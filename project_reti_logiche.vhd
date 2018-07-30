e8da8ab84e4833d9be2d9180d98707c00b9c6430
Adriano Mundo <37589421+adrianomundo@users.noreply.github.com>
2018-07-30 22:26:59 +0200

    Add files via upload
    update code vhdl

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -30,15 +30,15 @@ end project_reti_logiche;
 
 architecture FSM of project_reti_logiche is
     
-    type state_type is (START, SET_ADDRESS, READ_FROM_MEM, CHECK_INDEX_COL, CHECK_INDEX_ROW, SET_WRITE, SET_SQUARE, SET_MSB, SET_LSB, SET_DONE, UNSET_DONE); 
+    type state_type is (IDLE, START, SET_ADDRESS, READ_FROM_MEM, CHECK_INDEX_COL, CHECK_INDEX_ROW, SET_NEXT, SET_WRITE, SET_SQUARE, SET_MSB, SET_LSB, SET_DONE, UNSET_DONE); 
     signal next_state, current_state : state_type;
     
     signal width, height, threshold : unsigned (7 downto 0);
     signal c_min, c_max, r_min, r_max : unsigned (7 downto 0);
     signal col_index, row_index : unsigned (7 downto 0);
-    signal addr_counter, max_address, square : unsigned (15 downto 0);
+    signal max_address, square, addr_counter : unsigned (15 downto 0);
     signal c_min_assigned, r_min_assigned : std_logic;
-    
+
 begin
 
     state_reg: process(i_clk, i_rst) 
@@ -50,20 +50,22 @@ begin
         end if;
     end process;
 
-    lambda_delta: process(current_state, i_data,i_start)
+    lambda_delta: process(current_state, i_data, i_start)
     begin
         case current_state is
-            when START => 
+            when IDLE => 
+                addr_counter <= (1 => '1', others => '0');
+                c_min_assigned <= '0';
+                r_min_assigned <= '0';
+                col_index <= (others => '0');
+                row_index <= (others => '0');
+                next_state <= START;
+            when START =>
                 if (i_start = '1') then
-                    addr_counter <= (others =>'0');
-                    col_index <= (others => '0');
-                    row_index <= (others => '0');
-                    c_min_assigned <= '0';
-                    r_min_assigned <= '0';
-                    next_state <= SET_ADDRESS;
-                 else 
-                    next_state <= START;   
-                end if;
+                    next_state <= START;
+                else 
+                    next_state <= SET_ADDRESS; 
+                end if;           
             when SET_ADDRESS =>
                 o_en <= '1';
                 o_we <= '0';
@@ -74,28 +76,21 @@ begin
                     width <= unsigned(i_data);
                     addr_counter <= addr_counter + 1;
                     next_state <= SET_ADDRESS;
-                elsif (addr_counter = "00000000000000011") then
+                elsif (addr_counter = "0000000000000011") then
                     height <= unsigned(i_data);
                     addr_counter <= addr_counter + 1;
                     next_state <= SET_ADDRESS;
                 elsif (addr_counter = "0000000000000100") then
-                    max_address <= unsigned(width * height); 
+                    max_address <= unsigned(width * height) + 4; 
                     threshold <= unsigned(i_data);
                     addr_counter <= addr_counter + 1;
                     next_state <= SET_ADDRESS;
-                elsif (addr_counter = max_address + 1) then
-                    next_state <= SET_SQUARE;
+                elsif (addr_counter = (max_address + 1)) then
+                    o_en <= '0';
+                    next_state <= SET_WRITE;
                 else 
-                    if (col_index = width) then
-                        col_index <= (others => '0');
-                        row_index <= row_index + 1;
-                        addr_counter <= addr_counter + 1;
-                        next_state <= CHECK_INDEX_COL;
-                    else
-                        col_index <= col_index + 1;
-                        addr_counter <= addr_counter + 1;
-                        next_state <= CHECK_INDEX_COL;
-                    end if;
+                -- potrei aumentare addr_counter qui
+                    next_state <= CHECK_INDEX_COL;
                 end if;
             when CHECK_INDEX_COL =>
                if (i_data >= std_logic_vector(threshold)) then
@@ -104,58 +99,70 @@ begin
                        c_max <= col_index;
                        c_min_assigned <= '1';
                        next_state <= CHECK_INDEX_ROW;
-                   elsif (c_min_assigned = '1') then
-                          if (col_index < c_min) then
-                              c_min <= col_index;
-                              next_state <= CHECK_INDEX_ROW;
-                          elsif (col_index > c_max) then
-                              c_max <= col_index;
-                              next_state <= CHECK_INDEX_ROW;
-                          else
-                              next_state <= CHECK_INDEX_ROW;              
-                          end if;
+                   -- cmin assigned = 1
+                   else
+                       if (col_index < c_min) then
+                           c_min <= col_index;
+                           next_state <= CHECK_INDEX_ROW;
+                       elsif (col_index > c_max) then
+                           c_max <= col_index;
+                           next_state <= CHECK_INDEX_ROW;
+                       else
+                          next_state <= CHECK_INDEX_ROW;              
+                       end if;                           
                    end if;    
                else 
-                    next_state <= READ_FROM_MEM;    
+                    next_state <= SET_NEXT;    
                end if; 
             when CHECK_INDEX_ROW =>
                 if (r_min_assigned = '0') then
                     r_min <= row_index;
                     r_max <= row_index;
                     r_min_assigned <= '1';
-                    next_state <= SET_WRITE;
+                    next_state <= SET_NEXT;
                 else
                      if (row_index < r_min) then
                          r_min <= row_index;
-                         next_state <= SET_WRITE;  
+                         next_state <= SET_NEXT;  
                      elsif (row_index > r_max) then
                          r_max <= row_index;
-                         next_state <= SET_WRITE;
+                         next_state <= SET_NEXT;
                      else
-                        next_state <= SET_WRITE;    
+                        next_state <= SET_NEXT;    
                      end if;    
                 end if;
+            when SET_NEXT =>
+                if (col_index = width) then
+                    col_index <= (others => '0');
+                    row_index <= row_index + 1;
+                    addr_counter <= addr_counter + 1;
+                    next_state <= SET_ADDRESS;
+                else
+                    col_index <= col_index + 1;
+                    addr_counter <= addr_counter + 1;
+                    next_state <= SET_ADDRESS;
+                end if;         
             when SET_WRITE =>
                 o_en <= '1';
                 o_we <= '1';
                 next_state <= SET_SQUARE;
             when SET_SQUARE =>
                 square <= (c_max - c_min + 1) * (r_max - r_min + 1);    
-                o_address <= "0000000000000000";
+                o_address <= (0 => '1', others => '0');
                 next_state <= SET_MSB;
             when SET_MSB =>
-                o_data <= std_logic_vector(square(7 downto 0));
-                o_address <= "0000000000000001";
+                o_data <= std_logic_vector(square(15 downto 8));
+                o_address <= (others => '0');
                 next_state <= SET_LSB;
             when SET_LSB =>
-                o_data <= std_logic_vector(square(15 downto 8));
+                o_data <= std_logic_vector(square(7 downto 0));
                 next_state <= SET_DONE;
             when SET_DONE =>
                 o_done <= '1';
                 next_state <= UNSET_DONE;            
             when UNSET_DONE =>
                 o_done <= '0';
-                next_state <= START;    
+                --next_state <= START;    
         end case;
     end process;
     
